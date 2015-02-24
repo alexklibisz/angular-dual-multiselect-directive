9c77d9d654377f3269c579d9b64796ab7cd51095
Alex Klibisz <aklibisz@utk.edu>
2015-02-24 13:15:06 -0500

    re-added bootstrap and moved html to template variable

--- assets/DemoController.js
+++ assets/DemoController.js
@@ -5,8 +5,10 @@ demo.controller('DemoController',  function($scope, $location) {
 		filterPlaceHolder: 'Start typing to filter the lists below.',
 		labelAll: 'All Items',
 		labelSelected: 'Selected Items',
+		helpMessage: ' Click items to transfer them between fields.',
+		orderProperty: 'name',
 		items: [{"id": "50", "name": "Germany"}, {"id": "45", "name": "Spain"}, {"id": "66", "name": "Italy"}, {"id": "30", "name" : "Brazil" }, {"id": "41", "name": "France" }],
 		selectedItems: [] 
 	};
 
-}); //  StaticPageController
+});
--- assets/dual-multiselect-directive.js
+++ assets/dual-multiselect-directive.js
@@ -25,6 +25,6 @@ a = angular.module("dualmultiselect", []), a.directive("dualmultiselect", [funct
 				}
 			};
 		},
-		templateUrl: 'assets/template.html'
+		template: '<div class="dualmultiselect"> <div class="row"> <div class="col-lg-12 col-md-12 col-sm-12"> <h4>{{options.title}}<small>{{options.helpMessage}}</small> </h4> <input class="form-control" placeholder="{{options.filterPlaceHolder}}" ng-model="searchTerm"> </div></div><div class="row"> <div class="col-lg-6 col-md-6 col-sm-6"> <label>{{options.labelAll}}</label> <button class="btn btn-default btn-xs" ng-click="transfer(options.items, options.selectedItems, -1)"> Select All </button> <div class="pool"> <ul> <li ng-repeat="item in options.items | filter: searchTerm | orderBy: options.orderProperty"> <a href="" ng-click="transfer(options.items, options.selectedItems, options.items.indexOf(item))">{{item.name}}</a> </li></ul> </div></div><div class="col-lg-6 col-md-6 col-sm-6"> <label>{{options.labelSelected}}</label> <button class="btn btn-default btn-xs" ng-click="transfer(options.selectedItems, options.items, -1)"> Deselect All </button> <div class="pool"> <ul> <li ng-repeat="item in options.selectedItems | filter: searchTerm | orderBy: options.orderProperty"> <a href="" ng-click="transfer(options.selectedItems, options.items, options.selectedItems.indexOf(item))">{{item.name}}</a> </li></ul> </div></div></div></div>'
 	};
 }]);
