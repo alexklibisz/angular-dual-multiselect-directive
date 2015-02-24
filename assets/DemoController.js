a92b84d94d0b63027b3754986a86503f8fb1e587
Alex Klibisz <aklibisz@utk.edu>
2015-02-24 15:50:29 -0500

    finished demo page

--- assets/DemoController.js
+++ assets/DemoController.js
@@ -1,13 +1,13 @@
 demo.controller('DemoController',  function($scope, $location) {
 
 	$scope.demoOptions = {
-		title: 'Test',
+		title: 'Demo: Recent World Cup Winners',
 		filterPlaceHolder: 'Start typing to filter the lists below.',
 		labelAll: 'All Items',
 		labelSelected: 'Selected Items',
 		helpMessage: ' Click items to transfer them between fields.',
 		orderProperty: 'name',
-		items: [{"id": "50", "name": "Germany"}, {"id": "45", "name": "Spain"}, {"id": "66", "name": "Italy"}, {"id": "30", "name" : "Brazil" }, {"id": "41", "name": "France" }],
+		items: [{'id': '50', 'name': 'Germany'}, {'id': '45', 'name': 'Spain'}, {'id': '66', 'name': 'Italy'}, {'id': '30', 'name' : 'Brazil' }, {'id': '41', 'name': 'France' }, {'id': '34', 'name': 'Argentina'}],
 		selectedItems: [] 
 	};
 
